cdfcf2c60d8d928a8ce121023302fa3f079e31a7
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-01 17:57:54 +0530

    Initial Commit

---
+++ sum of 2 numbers.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : sum.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int number1,number2,sum;
+	printf("Enter two numbers"); /* prints enter two numbers */
+	fflush( stdout );/*for printf to execute before scanf  */
+	scanf("%d%d",&number1,&number2);
+	sum=number1+number2;
+	printf("Result is : %d",sum);
+
+	return EXIT_SUCCESS;
+}
