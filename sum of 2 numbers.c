66040d480fcf70b07c7afb9760579e054ff3b34f
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-01 18:11:16 +0530

    added name

--- sum of 2 numbers.c
+++ sum of 2 numbers.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : sum.c
- Author      : 
+ Author      : Sudeep Sudhevan
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
